dc936197a402292219bc3b73b7e4158365ed2ac5
Elijah Saltzman <elisaltzman@gmail.com>
2022-06-18 18:44:04 -0400

    fixed timed results checker

--- fire.py
+++ fire.py
@@ -28,12 +28,10 @@ else:
 #
 
 def callAPI():
-    threading.Timer(3600, callAPI).start()
     api_url = "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Current_WildlandFire_Perimeters/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
     response = requests.get(api_url)
     all = response.json()
     fires = all['features']
-    print('checked api')
     return fires
 
 # distance formula
@@ -146,206 +144,192 @@ for i in range(1,len(cwcoords)):
     cwmilemarkers[cwcoords[i]] = distance + d
     distance += d
 
-# Initialize text to be sent as SMS
+def main():
 
-text = ""
+    threading.Timer(3600, main).start()
 
-# Accessing fire boundary data from Wildland Fire Interagency Geospatial Services API every 1 hours
+    # Initialize text to be sent as SMS
 
-fires = callAPI()
+    text = ""
 
-# Accessing CO county geometry data
+    # Accessing fire boundary data from Wildland Fire Interagency Geospatial Services API every 1 hours
 
-# api_url = "https://www.cohealthmaps.dphe.state.co.us/arcgis/rest/services/OPEN_DATA/cdphe_geographic_analysis_boundaries/MapServer/5/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-# response = requests.get(api_url)
-# all = response.json()
-# counties = all['features']
-# #county name = counties[i]['attributes']['LABEL']
-# #county coords = counties[i]['geometry']['rings'][0]
+    fires = callAPI()
 
-# Check if fires are in CO, add fire info (name)
+    # Accessing CO county geometry data
 
-cofires = []
-cofireshapes = []
-for fire in fires:
-    inco = False
-    listofcoords = []
-    for key in fire:
-        coords = fire['geometry']['rings']
-        for coordlist in coords:
-            for coord in coordlist:
-                if(coord[1] > 37 and coord[1] < 41 and coord[0] > -109.05 and coord[0] < -102.05):
-                    listofcoords.append(coord)
-                    inco = True
-    if(inco == True):
-        cofires.append(fire)
-        cofireshapes.append(listofcoords)
+    # api_url = "https://www.cohealthmaps.dphe.state.co.us/arcgis/rest/services/OPEN_DATA/cdphe_geographic_analysis_boundaries/MapServer/5/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+    # response = requests.get(api_url)
+    # all = response.json()
+    # counties = all['features']
+    # #county name = counties[i]['attributes']['LABEL']
+    # #county coords = counties[i]['geometry']['rings'][0]
 
-# Creating test fire to check trail crossing function
+    # Check if fires are in CO, add fire info (name)
 
-# listofcoords = [(-107.5,37.6), (-107.5, 37.9), (-107.8, 37.9), (-107.8,37.6)]
-# cofireshapes.append(listofcoords)
-# testfire = {'attributes': {'irwin_IncidentName': 'Test', 'poly_GISAcres': 666}}
-# testfire['attributes']['irwin_IncidentName'] = 'Test'
-# testfire['attributes']['poly_GISAcres'] = 666
-# cofires.append(testfire)
+    cofires = []
+    cofireshapes = []
+    for fire in fires:
+        inco = False
+        listofcoords = []
+        for key in fire:
+            coords = fire['geometry']['rings']
+            for coordlist in coords:
+                for coord in coordlist:
+                    if(coord[1] > 37 and coord[1] < 41 and coord[0] > -109.05 and coord[0] < -102.05):
+                        listofcoords.append(coord)
+                        inco = True
+        if(inco == True):
+            cofires.append(fire)
+            cofireshapes.append(listofcoords)
 
-# Add number of fires to text
+    # Add number of fires to text
 
-text += "Total CO Fires: "
-text += str(len(cofires))
-text += "\n"
+    text += "Total CO Fires: "
+    text += str(len(cofires))
+    text += "\n"
 
-# Add fire names and areas to text
+    # Add fire names and areas to text
 
-for fire in cofires:
-    text += str(fire['attributes']['irwin_IncidentName'])
-    text += ' Fire - '
-    if fire['attributes']['poly_GISAcres'] is not None:
-        text += str(round(fire['attributes']['poly_GISAcres']))
-    else:
-        text += "N.A."
-    text += " acres, contaiment: "
-    if fire['attributes']['irwin_PercentContained'] is not None:
-        text += str(round(fire['attributes']['irwin_PercentContained']))
-        text += "%\n"
-    else:
-        text += "N.A.\n"
+    for fire in cofires:
+        text += str(fire['attributes']['irwin_IncidentName'])
+        text += ' Fire - '
+        if fire['attributes']['poly_GISAcres'] is not None:
+            text += str(round(fire['attributes']['poly_GISAcres']))
+        else:
+            text += "N.A."
+        text += " acres, contaiment: "
+        if fire['attributes']['irwin_PercentContained'] is not None:
+            text += str(round(fire['attributes']['irwin_PercentContained']))
+            text += "%\n"
+        else:
+            text += "N.A.\n"
 
-# Make fires list which contains shapely shapes of each fire
+    # Make fires list which contains shapely shapes of each fire
 
-for shape in cofireshapes:
-    poly = Polygon(shape)
-    x,y = poly.exterior.xy
-    plt.plot(x,y, color = "crimson")
+    for shape in cofireshapes:
+        poly = Polygon(shape)
+        x,y = poly.exterior.xy
+        plt.plot(x,y, color = "crimson")
 
-# Check if any fires cross the trail
+    # Check if any fires cross the trail
 
-ctlist = list(ct.coords)
-for i in range(len(ctlist)):
-    item = list(ctlist[i])
-    temp = item[0]
-    item[0] = item[1]
-    item[1] = temp
-    ctlist[i] = tuple(item)
+    ctlist = list(ct.coords)
+    for i in range(len(ctlist)):
+        item = list(ctlist[i])
+        temp = item[0]
+        item[0] = item[1]
+        item[1] = temp
+        ctlist[i] = tuple(item)
 
-ctline = LineString(ctlist)
-cross = False
-ncross = 0
-for i in range(len(cofireshapes)):
-    if(ctline.intersects(Polygon(cofireshapes[i]))):
-        cross = True
-        ncross += 1
-        crossline = ctline.intersection(Polygon(cofireshapes[i]))
-        x,y = crossline.xy
-        plt.plot(x,y, color = 'yellow')
-        crosslist = list(crossline.coords)
-        start = crosslist[0]
-        end = crosslist[len(crosslist)-1]
-        startlist = list(start)
-        endlist = list(end)
-        temp = startlist[0]
-        startlist[0] = startlist[1]
-        startlist[1] = temp
-        start = tuple(startlist)
-        temp = endlist[0]
-        endlist[0] = endlist[1]
-        endlist[1] = temp
-        end = tuple(endlist)
-        startmi = milemarkers[approx(start,list(milemarkers.keys()))]
-        endmi = milemarkers[approx(end,list(milemarkers.keys()))]
-        text += "The "
-        text += str(cofires[i]['attributes']['irwin_IncidentName'])
-        text += " Fire crosses the CT at mi. "
-        text += str(round(startmi))
-        if(abs(endmi - startmi) > 1):
-            text += " to mi. "
-            text += str(round(endmi))
-        text += "\n"
-        del cofireshapes[i]
-        del cofires[i]
+    ctline = LineString(ctlist)
+    cross = False
+    ncross = 0
+    for i in range(len(cofireshapes)):
+        if(ctline.intersects(Polygon(cofireshapes[i]))):
+            cross = True
+            ncross += 1
+            crossline = ctline.intersection(Polygon(cofireshapes[i]))
+            x,y = crossline.xy
+            plt.plot(x,y, color = 'yellow')
+            crosslist = list(crossline.coords)
+            start = crosslist[0]
+            end = crosslist[len(crosslist)-1]
+            startlist = list(start)
+            endlist = list(end)
+            temp = startlist[0]
+            startlist[0] = startlist[1]
+            startlist[1] = temp
+            start = tuple(startlist)
+            temp = endlist[0]
+            endlist[0] = endlist[1]
+            endlist[1] = temp
+            end = tuple(endlist)
+            startmi = milemarkers[approx(start,list(milemarkers.keys()))]
+            endmi = milemarkers[approx(end,list(milemarkers.keys()))]
+            text += "The "
+            text += str(cofires[i]['attributes']['irwin_IncidentName'])
+            text += " Fire crosses the CT at mi. "
+            text += str(round(startmi))
+            if(abs(endmi - startmi) > 1):
+                text += " to mi. "
+                text += str(round(endmi))
+            text += "\n"
+            del cofireshapes[i]
+            del cofires[i]
 
 
-if cross == False:
-    text += "0 fires cross the CT\n"
-if cross == True:
-    text += str(ncross)
-    text += " fires cross the CT\n"
+    if cross == False:
+        text += "0 fires cross the CT\n"
+    if cross == True:
+        text += str(ncross)
+        text += " fires cross the CT\n"
 
-# If fires do not cross the trail, find their closest point to the trail
+    # If fires do not cross the trail, find their closest point to the trail
 
-closestpoints = []
-for i in range(len(cofireshapes)):
-    distancebetween = []
-    for j in range(len(cofireshapes[i])-1):
-        distancebetween.append(getdistance(cofireshapes[i][j][0],cofireshapes[i][j][1],cofireshapes[i][j+1][0],cofireshapes[i][j+1][1]))
-    reducefactor = round(0.1/(sum(distancebetween)/len(distancebetween)))
-    lowresct = []
-    lowresfire = []
-    lowrescw = []
-    if(reducefactor > 0):
-        for k in range(0,len(cofireshapes[i]),reducefactor):
-            lowresfire.append(cofireshapes[i][k])
-    else:
-        for k in range(len(cofireshapes[i])):
-            lowresfire.append(cofireshapes[i][k])
-    for l in range(0,len(ctcoords),2):
-        lowresct.append(ctcoords[l])
-    for m in range(0,len(cwcoords),2):
-        lowrescw.append(cwcoords[m])
-    closestpoints.append([closest(lowresfire,lowresct),closest(lowresfire,lowrescw)])
+    closestpoints = []
+    for i in range(len(cofireshapes)):
+        distancebetween = []
+        for j in range(len(cofireshapes[i])-1):
+            distancebetween.append(getdistance(cofireshapes[i][j][0],cofireshapes[i][j][1],cofireshapes[i][j+1][0],cofireshapes[i][j+1][1]))
+        reducefactor = round(0.1/(sum(distancebetween)/len(distancebetween)))
+        lowresct = []
+        lowresfire = []
+        lowrescw = []
+        if(reducefactor > 0):
+            for k in range(0,len(cofireshapes[i]),reducefactor):
+                lowresfire.append(cofireshapes[i][k])
+        else:
+            for k in range(len(cofireshapes[i])):
+                lowresfire.append(cofireshapes[i][k])
+        for l in range(0,len(ctcoords),2):
+            lowresct.append(ctcoords[l])
+        for m in range(0,len(cwcoords),2):
+            lowrescw.append(cwcoords[m])
+        closestpoints.append([closest(lowresfire,lowresct),closest(lowresfire,lowrescw)])
 
-# show line from fire to closest point on trail
+    # show line from fire to closest point on trail
 
-for i in range(len(closestpoints)):
-    for j in range(len(closestpoints[i])):
-        fireline = []
-        fireline.append(tuple(closestpoints[i][j][1]))
-        swap = []
-        swap.append(closestpoints[i][j][2][1])
-        swap.append(closestpoints[i][j][2][0])
-        fireline.append(tuple(swap))
-        fl = LineString(fireline)
-        x,y = fl.xy
-        plt.plot(x,y,linestyle = 'dotted', color = "red")
+    for i in range(len(closestpoints)):
+        for j in range(len(closestpoints[i])):
+            fireline = []
+            fireline.append(tuple(closestpoints[i][j][1]))
+            swap = []
+            swap.append(closestpoints[i][j][2][1])
+            swap.append(closestpoints[i][j][2][0])
+            fireline.append(tuple(swap))
+            fl = LineString(fireline)
+            x,y = fl.xy
+            plt.plot(x,y,linestyle = 'dotted', color = "red")
 
-for i in range(len(closestpoints)):
-    if(closestpoints[i][0][0] < 50):
-        text += str(cofires[i]['attributes']['irwin_IncidentName'])
-        text += " Fire: "
-        text += str(round(closestpoints[i][0][0],1))
-        text += "mi from CT @ mi "
-        text += str(round(milemarkers[closestpoints[i][0][2]],1))
-        if(closestpoints[i][1][0] < 50):
-            text += " and "
+    for i in range(len(closestpoints)):
+        if(closestpoints[i][0][0] < 50):
+            text += str(cofires[i]['attributes']['irwin_IncidentName'])
+            text += " Fire: "
+            text += str(round(closestpoints[i][0][0],1))
+            text += "mi from CT @ mi "
+            text += str(round(milemarkers[closestpoints[i][0][2]],1))
+            if(closestpoints[i][1][0] < 50):
+                text += " and "
+                text += str(round(closestpoints[i][1][0],1))
+                text += "mi from CW alt @ mi "
+                text += str(round(cwmilemarkers[closestpoints[i][1][2]],1))
+        elif(closestpoints[i][1][0] < 50):
+            text += str(cofires[i]['attributes']['irwin_IncidentName'])
+            text += " Fire is: "
             text += str(round(closestpoints[i][1][0],1))
             text += "mi from CW alt @ mi "
             text += str(round(cwmilemarkers[closestpoints[i][1][2]],1))
-    elif(closestpoints[i][1][0] < 50):
-        text += str(cofires[i]['attributes']['irwin_IncidentName'])
-        text += " Fire is: "
-        text += str(round(closestpoints[i][1][0],1))
-        text += "mi from CW alt @ mi "
-        text += str(round(cwmilemarkers[closestpoints[i][1][2]],1))
-    text += "\n"
-    #print("The ", cofires[i]['attributes']['irwin_IncidentName'], " Fire is: ", closestpoints[i][0], "mi. from the Colorado trail at mile marker:", round(milemarkers[closestpoints[i][2]],1))
-
-# Send SMS update with Twilio
-
-# account_sid = "ACf8c276acb53197b719e5b9e07d89991d"
-# auth_token  = "8be95e1fd81a365058c4e084b86c00c0"
-
-# client = Client(account_sid, auth_token)
-
-print("number of characters:", len(text))
-print(text)
-#print(message.sid)
+        text += "\n"
+    
+    return text
 
 # Save visuals
 
-plt.savefig("fire.png", dpi = 200)
+# plt.savefig("fire.png", dpi = 200)
 
-
+text = main()
+print(text)
 
 #app = Flask(__name__)
 @app.route("/sms", methods=['POST'])
